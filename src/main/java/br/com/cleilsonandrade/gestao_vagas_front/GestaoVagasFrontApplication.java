f032c4769f82d68138997ab8e71928f8762f4789
CleilsonAndrade <cleilsonjose@hotmail.com>
2024-02-25 00:30:36 -0300

    ✨ feat: first commit

---
+++ src/main/java/br/com/cleilsonandrade/gestao_vagas_front/GestaoVagasFrontApplication.java
@@ -0,0 +1,13 @@
+package br.com.cleilsonandrade.gestao_vagas_front;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoVagasFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoVagasFrontApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/cleilsonandrade/gestao_vagas_front/GestaoVagasFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cleilsonandrade.gestao_vagas_front;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoVagasFrontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
