a5bd1526156c08e1b3ecb7b53b539a441b77a8ad
CleilsonAndrade <cleilsonjose@hotmail.com>
2024-03-05 18:23:51 -0300

    ✨ feat: security config

---
+++ src/main/java/br/com/cleilsonandrade/gestao_vagas_front/security/SecurityConfig.java
@@ -0,0 +1,24 @@
+package br.com.cleilsonandrade.gestao_vagas_front.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig {
+
+  @Bean
+  SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+    httpSecurity.authorizeHttpRequests(
+        auth -> {
+          auth.requestMatchers("/candidates/login").permitAll();
+          auth.anyRequest().authenticated();
+        })
+        .formLogin(form -> form.loginPage("/candidates/login"));
+
+    return httpSecurity.build();
+  }
+}
