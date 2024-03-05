cd8e1f12ab97916f0b2926305cfcdb466795c2e1
CleilsonAndrade <cleilsonjose@hotmail.com>
2024-03-05 16:40:28 -0300

    ✨ feat: candidate signIn controller and template

--- src/main/java/br/com/cleilsonandrade/gestao_vagas_front/modules/candidate/controller/CandidateController.java
+++ src/main/java/br/com/cleilsonandrade/gestao_vagas_front/modules/candidate/controller/CandidateController.java
@@ -2,7 +2,9 @@ package br.com.cleilsonandrade.gestao_vagas_front.modules.candidate.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/candidates")
@@ -11,6 +13,13 @@ public class CandidateController {
   @GetMapping("/login")
   public String login() {
     return "candidate/login";
+  }
+
+  @PostMapping("/signIn")
+  public String signIn(RedirectAttributes redirectAttributes, String username, String password) {
+
+    redirectAttributes.addAttribute("error_message", "Usuário/Senha incorreto(s)");
+    return "redirect:/candidate/login";
   }
 
 }
