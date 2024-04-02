213ebb368f33548f1d73677442b349551f417d46
CleilsonAndrade <cleilsonjose@hotmail.com>
2024-04-02 12:58:27 -0300

    📑 docs(readme): added

--- src/main/java/br/com/cleilsonandrade/gestaovagasweb/modules/candidate/service/CandidateService.java
+++ src/main/java/br/com/cleilsonandrade/gestaovagasweb/modules/candidate/service/CandidateService.java
@@ -3,6 +3,7 @@ package br.com.cleilsonandrade.gestaovagasweb.modules.candidate.service;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -13,6 +14,9 @@ import br.com.cleilsonandrade.gestaovagasweb.modules.candidate.dto.TokenDto;
 
 @Service
 public class CandidateService {
+  @Value("${base.url}")
+  private String baseUrl;
+
   public TokenDto login(String username, String password) {
     RestTemplate restTemplate = new RestTemplate();
 
@@ -26,7 +30,7 @@ public class CandidateService {
 
     HttpEntity<Map<String, String>> request = new HttpEntity<>(data, headers);
 
-    var result = restTemplate.postForObject("http://localhost:8080/candidates/auth", request, TokenDto.class);
+    var result = restTemplate.postForObject(baseUrl + "/candidates/auth", request, TokenDto.class);
 
     return result;
   }
--- src/main/java/br/com/cleilsonandrade/gestaovagasweb/modules/candidate/service/ProfileCandidateService.java
+++ src/main/java/br/com/cleilsonandrade/gestaovagasweb/modules/candidate/service/ProfileCandidateService.java
@@ -2,6 +2,7 @@ package br.com.cleilsonandrade.gestaovagasweb.modules.candidate.service;
 
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -10,6 +11,9 @@ import org.springframework.web.client.RestTemplate;
 
 @Service
 public class ProfileCandidateService {
+  @Value("${base.url}")
+  private String baseUrl;
+
   public String execute(String token) {
     RestTemplate restTemplate = new RestTemplate();
     HttpHeaders headers = new HttpHeaders();
@@ -17,7 +21,7 @@ public class ProfileCandidateService {
 
     HttpEntity<Map<String, String>> request = new HttpEntity<>(headers);
 
-    var result = restTemplate.exchange("http://localhost:8080/candidates", HttpMethod.GET, request,
+    var result = restTemplate.exchange(baseUrl + "/candidates", HttpMethod.GET, request,
         String.class);
     return result.getBody();
   }
